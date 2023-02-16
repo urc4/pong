b96c34455752f51c2e0c618d0cf8a6d134f80d39
Beato <121357043+urc4@users.noreply.github.com>
2023-02-16 15:51:52 -0300

    Draw paddles
    InnerHeight and InnerWidth were not working for some reason then I
    magically fixed it (the scrollbars showing up issue related) without
    changing any line of code.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 100, 100);
+class Paddle {
+  constructor({ position }) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 10;
+    this.height = 100;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const paddleOne = new Paddle({
+  position: {
+    x: 10,
+    y: 100,
+  },
+});
+
+const paddleTwo = new Paddle({
+  position: {
+    x: canvas.width - 10 * 2,
+    y: 100,
+  },
+});
+
+paddleOne.draw();
+paddleTwo.draw();
